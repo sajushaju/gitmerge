2d411b326ecdbfeaa8e77d54035260865892ddca
sajushaju <sajnatvpz@gmail.com>
2026-04-03 16:33:12 +0530

    modify login.py

---
+++ login.py
@@ -0,0 +1,8 @@
+user="admin"
+p="123456"
+username=input("enter your username: ")
+password=input("enter your password: ")
+if username==u and password==p:
+    print("login successful")
+else:
+    print("login failed")
