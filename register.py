3db34a7b2bfff8a3636bf56ca644701e272601c5
sajushaju <sajnatvpz@gmail.com>
2026-04-06 11:28:14 +0530

    developer2: updated user details

--- register.py
+++ register.py
@@ -1,9 +1,9 @@
 print('this is register module. please write register logic here')
-firstname = "developer_one"
-lastname = "team_lead"
-userType = "superadmin"
-username = "dev1_lead"
-password = "secure@789"
+firstname = "developer_two"
+lastname = "junior_dev"
+userType = "viewer"
+username = "dev2_junior"
+password = "pass@456"
 print("first name :",firstname)
 print("last name:",lastname)
 print("usertype:",userType)
